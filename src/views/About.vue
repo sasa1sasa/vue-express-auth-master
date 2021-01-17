82d8fbb43be58ab9559c8ad3f8c2727105a0af67
sasa1sasa <sasa1sasa02@gmail.com>
2021-01-17 19:56:06 +0100

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <div class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-size-1">About ALEX TAXi Services</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <p class="org-description is-size-4">
+        Thank`s for choose our Services we are Company with many Years
+        Experience with Cab,Airport Transfer,City Zürich Tours,Hospitaly
+        Delivery, .
+      </p>
+      <p class="org-description is-size-4">
+        We speak English and Germany Language you can call 24H every Day we are
+        allways here for you.Please take contact on the Website www.alex-taxi.ch
+      </p>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.org-description {
+  margin-top: 50px;
+}
+</style>
